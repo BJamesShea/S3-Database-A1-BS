275f404843f35d4926c8efa78a5fb3b5bfc98cdc
Brandon Shea <brandon.shea1994@icloud.com>
2025-01-28 18:22:47 -0330

    input logic

--- src/TemperatureAnalyzer.java
+++ src/TemperatureAnalyzer.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class TemperatureAnalyzer {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("How many temperatures do you want to enter?");
+        int numTemps = scanner.nextInt();
+        double[] temperatures = new double[numTemps];
+
+        for (int i = 0; i < numTemps ; i++) {
+            System.out.println("Enter temperature " + (i + 1) + ":");
+            temperatures[i] = scanner.nextDouble();
+        }
+
+        scanner.close();
+
+
+    }
 }
